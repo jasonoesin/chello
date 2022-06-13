6a710834cf99ef6a6f701ab9cc487c7f3d44fc22
Jason Oesin <91514784+jasonoesin@users.noreply.github.com>
2022-06-13 23:48:16 +0700

    Login Frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1 class="p-6 text-3xl font-bold underline">Test Aja</h1>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          JS - Learn React
-        </a>
-      </header> */}
+    <div className="App min-h-screen bg-grey-50 flex flex-col justify-center">
+      <div className="max-w-md w-full mx-auto">
+        <div className="text-center font-medium text-3xl">Log-in</div>
+      </div>
+      <div className="max-w-md w-full mx-auto mt-4 bg-white p-8 border border-gray-300">
+        <form action="" className="space-y-6">
+          <div className="">
+            <label htmlFor="" className="text-sm font-bold text-gray-600 block">
+              Email
+            </label>
+            <input
+              type="text"
+              className="w-full p-2 border border-gray-300 rounded mt-1"
+            />
+          </div>
+
+          <div className="">
+            <label htmlFor="" className="text-sm font-bold text-gray-600 block">
+              Password
+            </label>
+            <input
+              type="password"
+              className="w-full p-2 border border-gray-300 rounded mt-1"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                name=""
+                id=""
+                className="h-4 w-4 text-blue-300 rounded"
+              />
+              <label htmlFor="" className="ml-2 text-sm text-gray-600">
+                Remember Me
+              </label>
+            </div>
+          </div>
+          <div className="">
+            <button className="w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 rounded-md text-white text-sm">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
