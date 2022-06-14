9b8f55cc2f82d35249d3f14c28f17071a64bec3f
Jason Oesin <91514784+jasonoesin@users.noreply.github.com>
2022-06-15 01:55:37 +0700

    Dropdown

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom";
+import React from "react";
 
 const Navbar = () => {
+  function openDropdown(e) {
+    var drop = document.getElementById("dropdown-menu");
+
+    drop.classList.remove("hidden");
+  }
+
+  function closeDropdown() {
+    var drop = document.getElementById("dropdown-menu");
+
+    drop.classList.add("hidden");
+  }
+
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
@@ -12,12 +25,13 @@ const Navbar = () => {
         <div className="flex items-center md:order-2">
           <button
             type="button"
-            className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+            className="dropdown dropdown-toggle flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
             id="user-menu-button"
             aria-expanded="false"
             data-dropdown-toggle="dropdown"
+            onFocus={openDropdown}
+            onBlur={closeDropdown}
           >
-            <span className="sr-only">Open user menu</span>
             <img
               className="w-8 h-8 rounded-full"
               src="https://i.picsum.photos/id/66/200/200.jpg?hmac=gaKXe-rWmo5fSEm79TTkW_yFJLECw3FdRCh6Dm7jp4g"
@@ -73,6 +87,54 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+      </div>
+
+      <div
+        className="hidden w-64 absolute z-50 my-4 right-4 text-base list-none bg-white border rounded divide-y divide-gray-100 drop-shadow-xl dark:divide-gray-600"
+        id="dropdown-menu"
+      >
+        <div className="py-3 px-4">
+          <span className="block text-sm text-gray-900 dark:text-white">
+            Name
+          </span>
+          <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">
+            name@gmail.com
+          </span>
+        </div>
+        <ul className="py-1" aria-labelledby="dropdown">
+          <li>
+            <a
+              href="#"
+              className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+            >
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+            >
+              Settings
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+            >
+              Earnings
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+            >
+              Sign out
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
